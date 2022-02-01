978ab39ab4e81d949494509753052a644da18c7b
AviranLevi <aveloofficial@gmail.com>
2022-02-01 15:42:18 +0200

    minor fix

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
 export const DEV = false
 
-export const serverURL = process.env.REACT_APP_MY_STYLIST_URL || 'http://localhost:3030/api'
+export const serverURL = process.env.REACT_APP_SERVER_URL || 'http://localhost:3030/api'
